e3c3a3b806baa789414fb451c044d357fefb6a14
AlanAds03 <alan.ads03@gmail.com>
2015-12-29 00:11:26 -0200

    configurando framework

---
+++ App/MaisGamers/Frameworks/Conexao.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace Frameworks
+{
+    
+    public class Conexao<T> : IDisposable
+    {
+        
+        private static IDbConnection connection;
+        private static IDbCommand command;
+
+        public Conexao()
+        {
+            connection = new OdbcConnection("Data Source=.; DataBase=maisGamers;user=sa;pwd=qweasd");
+        }
+
+        public bool Conectar()
+        {
+            try
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+            
+
+        }
+
+        public IDataReader ExecuteReader(string query, List<IDbDataParameter> parameters )
+        {
+            IDataReader reader;
+            command.CommandText = query;
+            command.Connection = connection;
+
+            if (parameters.Any()) { 
+                foreach (var param in parameters)
+                {
+                    command.Parameters.Add(param);
+                }
+            }
+            reader = command.ExecuteReader();
+            return reader;
+
+        }
+
+        public DataTable ExecuteDataTable(string query, List<IDbDataParameter> parameters)
+        {
+
+
+            IDataReader reader;
+            command.CommandText = query;
+            command.Connection = connection;
+
+            DataTable table = new DataTable();
+            if (parameters.Any())
+            {
+                foreach (var param in parameters)
+                {
+                    command.Parameters.Add(param);
+                }
+            }
+
+            reader = command.ExecuteReader();
+            table.Load(reader);
+            return table;
+
+        }
+
+        public bool ExecuteNowQuery(string query, List<IDbDataParameter> parameters)
+        {
+
+
+            IDataReader reader;
+            command.CommandText = query;
+            command.Connection = connection;
+
+            DataTable table = new DataTable();
+            if (parameters.Any())
+            {
+                foreach (var param in parameters)
+                {
+                    command.Parameters.Add(param);
+                }
+            }
+
+            reader = command.ExecuteReader();
+            table.Load(reader);
+            return table;
+
+        }
+
+
+
+        public Object ExecuteScalar(string query, List<IDbDataParameter> parameters)
+        {
+            
+            command.CommandText = query;
+            command.Connection = connection;
+            if (parameters.Any())
+            {
+                foreach (var param in parameters)
+                {
+                    command.Parameters.Add(param);
+                }
+            }
+            return command.ExecuteScalar();
+        }
+
+        public void Dispose()
+        {
+            command = null;
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+}
---
+++ App/MaisGamers/MaisGamers/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MaisGamers
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pesquisarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastroToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orçamentoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.locaçãoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jogarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lançarHoraToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consolesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.produtosToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.produtosToolStripMenuItem,
+            this.clientesToolStripMenuItem,
+            this.locaçãoToolStripMenuItem,
+            this.jogarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(940, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.pesquisarToolStripMenuItem,
+            this.cadastrarToolStripMenuItem});
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.produtosToolStripMenuItem.Text = "&Produtos";
+            // 
+            // pesquisarToolStripMenuItem
+            // 
+            this.pesquisarToolStripMenuItem.Name = "pesquisarToolStripMenuItem";
+            this.pesquisarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.pesquisarToolStripMenuItem.Text = "Pesquisar";
+            // 
+            // cadastrarToolStripMenuItem
+            // 
+            this.cadastrarToolStripMenuItem.Name = "cadastrarToolStripMenuItem";
+            this.cadastrarToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cadastrarToolStripMenuItem.Text = "Cadastrar";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            this.clientesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastroToolStripMenuItem,
+            this.orçamentoToolStripMenuItem});
+            this.clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            this.clientesToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.clientesToolStripMenuItem.Text = "&Clientes";
+            // 
+            // cadastroToolStripMenuItem
+            // 
+            this.cadastroToolStripMenuItem.Name = "cadastroToolStripMenuItem";
+            this.cadastroToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.cadastroToolStripMenuItem.Text = "Cadastro";
+            this.cadastroToolStripMenuItem.Click += new System.EventHandler(this.cadastroToolStripMenuItem_Click);
+            // 
+            // orçamentoToolStripMenuItem
+            // 
+            this.orçamentoToolStripMenuItem.Name = "orçamentoToolStripMenuItem";
+            this.orçamentoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.orçamentoToolStripMenuItem.Text = "Orçamento";
+            // 
+            // locaçãoToolStripMenuItem
+            // 
+            this.locaçãoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.consolesToolStripMenuItem,
+            this.produtosToolStripMenuItem1});
+            this.locaçãoToolStripMenuItem.Name = "locaçãoToolStripMenuItem";
+            this.locaçãoToolStripMenuItem.Size = new System.Drawing.Size(63, 20);
+            this.locaçãoToolStripMenuItem.Text = "Locação";
+            // 
+            // jogarToolStripMenuItem
+            // 
+            this.jogarToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lançarHoraToolStripMenuItem});
+            this.jogarToolStripMenuItem.Name = "jogarToolStripMenuItem";
+            this.jogarToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.jogarToolStripMenuItem.Text = "Jogar";
+            // 
+            // lançarHoraToolStripMenuItem
+            // 
+            this.lançarHoraToolStripMenuItem.Name = "lançarHoraToolStripMenuItem";
+            this.lançarHoraToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.lançarHoraToolStripMenuItem.Text = "Lançar Hora";
+            // 
+            // consolesToolStripMenuItem
+            // 
+            this.consolesToolStripMenuItem.Name = "consolesToolStripMenuItem";
+            this.consolesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.consolesToolStripMenuItem.Text = "Consoles";
+            // 
+            // produtosToolStripMenuItem1
+            // 
+            this.produtosToolStripMenuItem1.Name = "produtosToolStripMenuItem1";
+            this.produtosToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.produtosToolStripMenuItem1.Text = "Produtos";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 344);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pesquisarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastrarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastroToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orçamentoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem locaçãoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jogarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem lançarHoraToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consolesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem1;
+    }
+}
+
